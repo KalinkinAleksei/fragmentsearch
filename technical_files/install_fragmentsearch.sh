acc1d687122d2b86668e39f0290d13286a90ffcc
KalinkinAleksei <kalinkin34kbk@gmail.com>
2025-01-27 04:05:56 +0300

    install eror resolved

--- technical_files/install_fragmentsearch.sh
+++ technical_files/install_fragmentsearch.sh
@@ -3,5 +3,5 @@
 wget https://mmseqs.com/foldseek/foldseek-linux-avx2.tar.gz
 tar xvzf foldseek-linux-avx2.tar.gz
 rm -f foldseek-linux-avx2.tar.gz
-foldseek databases PDB pdb tmp 
+./foldseek/bin/foldseek databases PDB pdb tmp 
 pip install numpy pandas
